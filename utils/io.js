190a9c117e30a641f7f80b99de38c99f1e8cde6b
Normaljun95 <jsj950629@gmail.com>
2023-11-24 01:32:08 +0900

    ✨Feat : 연결 끊김 관련 console log 추가

--- utils/io.js
+++ utils/io.js
@@ -1,5 +1,10 @@
 module.exports = function (io) {
     io.on("connection", async(socket) => {
         console.log("client is connected", socket.id);
+
+        // 연결 끊김 관련 console log 추가 (11.24)
+        socket.on("disconnected", () => {
+            console.log("user is disconnected");
+        });
     });
 };
